5fea9ccab3a20bf236a06eb0a8e2ceae51ed7f6b
Shohra Qadri <qadrishohra@gmail.com>
2024-05-27 11:38:14 +0530

    navbar responsive

--- instagram/src/components/Navbar/Navbar.jsx
+++ instagram/src/components/Navbar/Navbar.jsx
@@ -2,9 +2,10 @@
 import React, { useContext } from 'react';
 import { Link, useNavigate } from 'react-router-dom'; // Import Link component
 import './Navbar.css';
-import logo from '../../img/Logo-Instagram.webp';
+import logo from '../../assets/inta-icon.webp';
 import { LoginContext } from '../../context/LoginContext';
-
+import { IoMdHome, IoIosLogOut, IoIosAddCircle } from "react-icons/io";
+import { MdAccountCircle, MdExplore } from "react-icons/md";
 
 // eslint-disable-next-line react/prop-types
 function Navbar({ login }) {
@@ -42,15 +43,16 @@ function Navbar({ login }) {
         if (login || token) {
             return (
                 <>
-                    <li key="profile"><Link to="/profile">Profile</Link></li>
-                    <li key="followingpost"><Link to="/followingpost">My Following</Link></li>
-                    <li key="createpost"><Link to="/createpost">Createpost</Link></li>
+                    <li key="profile"><Link to="/"><IoMdHome /></Link></li>
+                    <li key="profile"><Link to="/profile"><MdAccountCircle /></Link></li>
+                    <li key="followingpost"><Link to="/followingpost"><MdExplore /></Link></li>
+                    <li key="createpost"><Link to="/createpost"><IoIosAddCircle /></Link></li>
                     <Link to={""}>
-                        <button className='primaryBtn border-2 cursor-pointer font-bold' onClick={() =>
+                        <link onClick={() =>
                             setmodalOpen(true)
                         }>
-                            Log Out
-                        </button>
+                            <IoIosLogOut />
+                        </link>
                     </Link>
                 </>
             );
@@ -67,9 +69,12 @@ function Navbar({ login }) {
 
     return (
         <div className='Navbar flex justify-around items-center '>
-            <img src={logo} alt="" className='object-contain' onClick={() => { Navigate("/") }} />
+            <img src={logo} alt="" id="instalogo" className='object-contain' onClick={() => { Navigate("/") }} />
             <ul className="menubar flex gap-2 items-center">
                 {loginStatus()}
+            </ul>
+            <ul className=" nav-mobile flex gap-2 items-center">
+                {loginStatusMobile()}
             </ul>
         </div>
     );
